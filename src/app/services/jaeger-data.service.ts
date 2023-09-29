50e7a74840234efefc450d0f07bba9ee9b12f2ba
Drimmelen, W. van (Wesley) <Wesley.vanDrimmelen@devolksbank.nl>
2023-09-29 10:06:30 +0200

    Fix getProcesses

--- src/app/services/jaeger-data.service.ts
+++ src/app/services/jaeger-data.service.ts
@@ -17,14 +17,14 @@ export class JaegerDataService {
     private readonly _http: HttpClient
   ) { }
 
-  getProcesses(ranking?: Ranking): Observable<Process[]> {
+  getProcesses(ranking: Ranking): Observable<Process[]> {
     // return this._http.get<Process[]>('/assets/mock-data/proces_list_mock.json').pipe(
     //   map((processes: Process[]) => processes.sort((a, b) => 
     //     a.rank < b.rank ? 1 : (a.rank === b.rank ? 0 : -1)
     //   ))
     // )
     debug(`Calling get_process_list(${ranking})`);
-    return from(invoke<Process[]>('get_process_list', {metric: ranking})).pipe(
+    return from(invoke<Process[]>('get_process_list', {metric: ranking.value})).pipe(
         tap((graphs) => {
           info(`Returned from RUST: process_list with: lenght ${graphs.length}`);
         })
