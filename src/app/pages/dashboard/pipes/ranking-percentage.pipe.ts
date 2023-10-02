524c9809cd6e1e43c7caaf7472f196152ae37d15
Drimmelen, W. van (Wesley) <Wesley.vanDrimmelen@devolksbank.nl>
2023-10-02 15:09:33 +0200

    remove logs

--- src/app/pages/dashboard/pipes/ranking-percentage.pipe.ts
+++ src/app/pages/dashboard/pipes/ranking-percentage.pipe.ts
@@ -7,11 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RankingPercentagePipe implements PipeTransform {
 
   transform(digit: number): number | string {
-    console.log(digit)
-    // if (digit === -1000) return digit;
-
-    console.log(digit);
-    
     return digit.toLocaleString('nl', { style: 'percent', minimumFractionDigits: 1 })
   }
 
